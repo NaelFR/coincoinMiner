b1a1271b3dcd31018dc13287a954c30f2aa9be37
Nael <abou.chahine.nael@gmail.com>
2017-12-11 18:34:43 +0100

    first files

---
+++ headers/Token.h
@@ -0,0 +1,15 @@
+//
+// Created by Nael Abou Chahine on 11/12/2017.
+//
+
+#ifndef COINCOIN_MINER_TOKEN_H
+#define COINCOIN_MINER_TOKEN_H
+
+
+class Token {
+public:
+
+};
+
+
+#endif //COINCOIN_MINER_TOKEN_H
---
+++ source/Token.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nael Abou Chahine on 11/12/2017.
+//
+
+#include "../headers/Token.h"
